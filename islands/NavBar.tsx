d17ccc3a5f485c75c982b47be78069781c504245
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-09-07 13:39:12 -0400

    correccion en las animaciones

--- islands/NavBar.tsx
+++ islands/NavBar.tsx
@@ -2,15 +2,28 @@ import { useState } from "preact/hooks"
 
 export default function NavBar(){
 
-    const [showMenu, setShowMenu] = useState<string>("hidden")
+    const [menuBackdropAnim, setMenuBackdropAnim] = useState<string>("hidden")
+    const [menuAnim, setMenuAnim] = useState<string>("hidden")
+    const [open, setOpen] = useState<boolean>(false)
+
+    const changeState = () => {
+        if(open === false){
+            setMenuBackdropAnim("blendIn")
+            setMenuAnim("showMenu")
+        }else{
+            setMenuBackdropAnim("blendOut")
+            setMenuAnim("hideMenu")
+        }
+        setOpen(!open)
+    }
 
     return(
         <div class="NavBar">
             <h4>Evelio</h4>
-            <img src="/menu-icon.png" class="menuIcon" onClick={() => setShowMenu("showMenu")}/>
-            <div class={`${showMenu} menuBackDrop`}>
-                <div class={`${showMenu} hiddenMenu`}>
-                    <img src="/menu-icon.png" class="menuIcon" onClick={() => setShowMenu("hideMenu")}/>
+            <img src="/menu-icon.png" class="menuIcon" onClick={changeState}/>
+            <div class={`${menuBackdropAnim} menuBackDrop`}>
+                <div class={`${menuAnim} menu`}>
+                    <img src="/menu-icon.png" class="menuIcon" onClick={changeState}/>
                     <a href="/">Inicio</a>
                     <a href="/Verify">Verificar tickets</a>
                     <a href="/Support">Soporte</a>
